3fc597c9cfacdc914846281a791cec26e4e87b7e
ZhongWenhui1995 <1165821317@qq.com>
2017-01-22 10:41:50 +0800

    SokobanSolver V1.0
    main函数修改

--- src/zwh/main/Start.java
+++ src/zwh/main/Start.java
@@ -14,13 +14,41 @@ import zwh.solver.judge.DefaultJudger;
 public class Start {
 
     public static void main(String[] args) {
+	run();
+//	runWithAverageTime();
+    }
+
+    private static void run() {
+	SokobanMap map = readMap();
+	ISokobanSolver solver = new ViolenceConcurrentSolver(new DefaultJudger());
+	// ISokobanSolver solver = new ViolentSingleSolver(new DefaultJudger());
+	if (MapChecker.isValidMap(map)) {
+	    try {
+		solver.solve(map);
+	    } catch (Exception e) {
+		e.printStackTrace();
+	    }
+	    SokobanMap resMap = solver.getSolvedMap();
+	    System.out.println("Time: " + solver.getSolvedTime());
+	    if (resMap != null) {
+		System.out.println(resMap.path);
+	    } else {
+		System.out.println("can not find the way to solve the SokobanMap");
+	    }
+	} else {
+	    System.out.println("this is not valid sokoban map");
+	}
+    }
+
+    private static void runWithAverageTime() {
 	long time = 0;
 	final int count = 100;
 	int successCount = 0;
 	SokobanMap map = readMap();
 	for (int i = 0; i < count; i++) {
-//	    ISokobanSolver solver = new ViolenceConcurrentSolver(new DefaultJudger());
-	     ISokobanSolver solver = new ViolentSingleSolver(new DefaultJudger());
+	    // ISokobanSolver solver = new ViolenceConcurrentSolver(new
+	    // DefaultJudger());
+	    ISokobanSolver solver = new ViolentSingleSolver(new DefaultJudger());
 	    if (MapChecker.isValidMap(map)) {
 		try {
 		    solver.solve(map);
